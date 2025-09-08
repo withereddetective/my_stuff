a656d1fa821ef19ffb84a86ce6d87eb4b9f1098c
withereddetective <fnafgameplay44@gmail.com>
2025-09-08 13:26:59 -0500

    asdfg

--- c++/personal/calcu latr.cpp
+++ c++/personal/calcu latr.cpp
@@ -7,7 +7,13 @@ int add(int num1, int num2){
     return num1 + num2;
 }
 
+int sub(int num1, int num2){
+    return num1 - num2;
+}
 
+int mul(int num1, int num2){
+    return num1 * num2;
+}
 
 int main(){
 
