eb0c6900eccf3bd36a0dc66b1677e225b5c96e6f
withereddetective <fnafgameplay44@gmail.com>
2025-09-08 11:13:21 -0500

    yeah

---
+++ c++/personal/calcu latr.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int add(int num1, int num2){
+    return num1 + num2;
+}
+
+
+
+int main(){
+
+    int num1, num2;
+    string opp;
+    cout << "what is your initial number";
+    cin >> num1;
+
+    while (true)
+    {
+        
+        cout << "what is the first 3 letters of your operation?";
+        cin >> opp;
+
+        cout << "what is the number you will operate with?";
+        cin >> num2;
+
+        
+
+    }
+    
+
+}
