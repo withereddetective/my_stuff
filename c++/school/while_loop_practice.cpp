3f5d87202bf3fe6302618027fd71c445462663d3
withereddetective <fnafgameplay44@gmail.com>
2025-09-25 20:52:10 -0500

    assignment for 9/25

---
+++ c++/school/while_loop_practice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // declare variable
+    int i = 0;
+
+    // set up while loop to cycle through 5
+    while (i <= 5){
+        
+        // print counter variable
+        cout << i << endl;
+
+        // increment counter variable up
+        i++;
+
+    }
+}
